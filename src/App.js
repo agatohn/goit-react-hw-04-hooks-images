f285316ebb8bdf249717b15e7ae58f2c82b2c8de
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-10-05 23:08:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
-import { Component } from "react";
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import SearchForm from "./Components/searchForm/SearchForm";
 import ImageGallery from "./Components/imageGallery/ImageGallery";
 import Modal from "./Components/modal/Modal";
 
-export default class App extends Component {
-  state = {
-    query: "",
-    page: 1,
-    showModal: false,
-    urlModal: "",
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [urlModal, setUrlModal] = useState("");
+
+  const handleFormSubmit = (query) => {
+    setQuery(query);
+    setPage(Number(1));
   };
 
-  handleFormSubmit = (query) => {
-    this.setState({ query, page: 1 });
-  };
-  handlePageIncr = () => {
-    this.setState({ page: Number(this.state.page) + 1 });
+  const handlePageIncr = () => {
+    setPage(page + 1);
   };
 
-  closeModal = () => {
-    this.setState({ urlModal: "", showModal: false });
+  const closeModal = () => {
+    setUrlModal("");
+    setShowModal(false);
   };
 
-  handleImageClick = (url) => {
-    this.setState({ urlModal: url, showModal: true });
+  const handleImageClick = (url) => {
+    setUrlModal(url);
+    setShowModal(true);
   };
-  //url needed
-  render() {
-    return (
-      <div>
-        <SearchForm onSubmit={this.handleFormSubmit} />
-        {this.state.showModal && (
-          <Modal url={this.state.urlModal} onClose={this.closeModal} />
-        )}
-        <ImageGallery
-          query={this.state.query}
-          page={this.state.page}
-          onOpen={this.handleImageClick}
-          handlePageIncr={this.handlePageIncr}
-        />
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <SearchForm onSubmit={handleFormSubmit} />
+      {showModal && <Modal url={urlModal} onClose={closeModal} />}
+      <ImageGallery
+        query={query}
+        page={page}
+        onOpen={handleImageClick}
+        handlePageIncr={handlePageIncr}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
 }
